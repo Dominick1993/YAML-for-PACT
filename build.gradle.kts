7a0357408235fb78733a68a07d2700daad5067fa
Dominik Labuda <dominikla@gmail.com>
2019-02-04 17:05:25 +0100

    Gradle initialized

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    application
 }
 
 repositories {
@@ -10,4 +11,17 @@ repositories {
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+dependencies {
+    // This dependency is found on compile classpath of this component and consumers.
+    implementation("com.google.guava:guava:27.0.1-jre")
+
+    // Use JUnit test framework
+    testImplementation("junit:junit:4.12")
+}
+
+application {
+    // Define the main class for the application
+    mainClassName = "com.labuda.yamlpact.App"
 }
