9515ca79873c9a147008762d610c3533af5ac4de
Dominik Labuda <dominikla@gmail.com>
2019-02-04 16:23:11 +0100

    Gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    java
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
